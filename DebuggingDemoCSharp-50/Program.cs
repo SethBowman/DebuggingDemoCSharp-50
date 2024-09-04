e049415b040ee62e53827cb24ac3a72348958d73
SethBowman <brandonsethbowman@gmail.com>
2024-09-04 10:27:29 -0500

    demo completed

---
+++ DebuggingDemoCSharp-50/Program.cs
@@ -0,0 +1,30 @@
+﻿
+//Compile-time errors
+//int number = "Seth";
+
+//runtime errors
+//throw exceptions
+
+// Console.WriteLine("Give me a number");
+//
+// var userInput = int.Parse(Console.ReadLine());
+//
+// Console.WriteLine(userInput);
+
+// var numbers = new int[] { 1, 2, 3, 4 };
+//
+// Console.WriteLine(numbers[10]);
+
+
+//logical errors
+
+var name = "bob";
+var age = 50;
+
+Console.WriteLine($"Hello my name is {age} and I am {name} years old");
+
+//<-- put a breakpoint here
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
